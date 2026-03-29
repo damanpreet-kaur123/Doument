df308b3027d57fb2d65977bb85545fe48a9e3538
damanpreet-kaur123 <155047827+damanpreet-kaur123@users.noreply.github.com>
2026-03-29 15:53:45 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import router from './routers/router.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+if(process.env.MONGO_URI) {
+    mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('Mongo Connected'))
+    .catch((err) => console.error(err));
+}
+
+app.use('/api', router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
